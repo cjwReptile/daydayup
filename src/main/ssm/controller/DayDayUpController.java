9be67ad9dabd7dac86ef4a858dce44efa6fe5ca8
cjwReptile <836362264@qq.com>
2017-06-09 18:19:07 +0800

    cjwcjw

--- src/main/ssm/controller/DayDayUpController.java
+++ src/main/ssm/controller/DayDayUpController.java
@@ -1,12 +1,20 @@
 package ssm.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ssm.interceptor.MethodCacheInterceptor;
 
 /**
  * Created by hasee on 2017/4/27.
  */
 @Controller
+@RequestMapping(value="/cjw")
 public class DayDayUpController {
-
+    @RequestMapping(value="/test")
+       public void test(){
+            System.out.print("sssssss");
+           MethodCacheInterceptor methodCacheInterceptor=new MethodCacheInterceptor();
+           methodCacheInterceptor.set();
+       }
 
 }
